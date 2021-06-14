74a1ce7676754fec704f04d849c84fbdb66aed33
Joaco <joacodm96@gmail.com>
2021-06-14 00:21:46 -0300

    heroe modal added

---
+++ src/components/HeroCard/HeroeModal/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {StyledModal} from "/StyledModal"
+
+export const HeroeModal = () =>{
+    return(
+        <StyledModal>
+            
+        </StyledModal>
+    )
+}
--- src/components/HeroCard/index.js
+++ src/components/HeroCard/index.js
@@ -1,24 +1,10 @@
-import React, { useEffect } from "react"
+import React from "react"
+import { StyledCard } from "./StyledCard"
 
- const accesKey = 'a17e0b037ef914943a9509824d4f1272';
-
-
-
-
-
-/* useEffect(() => {
-    const randomNumber = Math.round(Math.random() * 1493);
-    const response = await fetch(`http://gateway.marvel.com/v1/public/characters&apikey=${accesKey}&limit=8&offset=${randomNumber}`)
-    const data = await response.json();
-    return data;
+export const HeroeCard = () => {
+    return(
+        <StyledCard>
+            {props.name}
+        </StyledCard>
+    )
 }
-) */
-
-
-/* const search = async (query) => {
-    const response = await fetch(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&apikey=${accesKey}`)
-    const data = await response.json();
-    return data;
-}
-
-searchElement.addEventListener('enter') */
